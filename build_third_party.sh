328116e16b6c5cfd6dc5274851dfaa0149e5fb30
ANYKS <forman@anyks.com>
2024-09-22 03:22:01 +0300

    Реализация сборки зависимостей

---
+++ build_third_party.sh
@@ -0,0 +1,354 @@
+#!/usr/bin/env bash
+
+# Получаем корневую дирректорию
+readonly ROOT=$(cd "$(dirname "$0")" && pwd)
+
+# Получаем версию OS
+OS=$(uname -a | awk '{print $1}')
+
+# Флаг активации модуля IDN
+IDN="no"
+
+# Флаг активации режима отладки
+DEBUG="no"
+
+if [[ $OS =~ "MINGW64" ]]; then
+	OS="Windows"
+fi
+
+if [ -n "$1" ]; then
+	if [ $1 = "--clean" ] || [ $1 = "--reset" ]; then
+
+		# Очистка подпроекта
+		clean_submodule(){
+			cd "$ROOT/submodules/$1" || exit 1
+			git clean -dfx
+			git stash
+			cd "$ROOT" || exit 1
+		}
+
+		# Очистка директории
+		clean_directory(){
+			git clean -dfx "$1"
+		}
+
+		clean_submodule "yaml"
+		clean_submodule "libxml2"
+
+		if [ $1 = "--reset" ]; then
+			clean_submodule "awh"
+			"$ROOT/submodules/awh/build_third_party.sh" --clean
+		else
+			rm -rf "$ROOT/submodules/awh/build"
+			rm -f "$ROOT/submodules/awh/.stamp_done"
+		fi
+
+		# Удаляем сборочную директорию
+		rm -rf "$ROOT/third_party"
+
+		printf "\n****************************************"
+		printf "\n************   Success!!!   ************"
+		printf "\n****************************************"
+		printf "\n\n\n"
+
+		exit 0
+	elif [ $1 != "--update" ]; then
+		printf "Usage: config [options]\n"
+		printf " --clean - Cleaning all submodules and build directory\n"
+
+		exit 1
+	fi
+fi
+
+# Каталог для установки собранных библиотек
+PREFIX="$ROOT/third_party"
+export PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig"
+
+# Устанавливаем флаги глобального использования
+# export CPPFLAGS=""
+export CXXFLAGS="$CXXFLAGS -fPIC"
+export CFLAGS="-I$PREFIX/include -fPIC"
+
+export LDFLAGS="-L$PREFIX/lib"
+export LD_LIBRARY_PATH="$PREFIX/lib"
+
+# Создаём каталоги
+mkdir -p "$PREFIX/bin"
+mkdir -p "$PREFIX/lib"
+mkdir -p "$PREFIX/include"
+
+# Определяем количество логических ядер
+if [ $OS = "Darwin" ]; then
+	# Устанавливаем количество ядер системы
+	numproc=$(sysctl -n hw.logicalcpu)
+	# Если версия MacOS X не установлена
+	if [ ! -n "$MACOSX_DEPLOYMENT_TARGET" ]; then
+		# Устанавливаем версию операционной системы
+		export MACOSX_DEPLOYMENT_TARGET=$(sw_vers -productVersion)
+	fi
+elif [ $OS = "FreeBSD" ]; then
+	# Устанавливаем количество ядер системы
+	numproc=$(sysctl -n hw.ncpu)
+else
+	# Устанавливаем количество ядер системы
+	numproc=$(nproc)
+fi
+
+if [ $OS = "Darwin" ]; then # MacOS
+	INSTALL_CMD="ditto -v"
+elif [ $OS = "FreeBSD" ]; then # FreeBSD
+	INSTALL_CMD="install -m 0644"
+elif [ $OS = "Windows" ]; then # Windows
+	INSTALL_CMD="install -D -m 0644"
+else # Linux
+	INSTALL_CMD="install -D -m 0644"
+fi
+
+# Применяем патчи
+apply_patch(){
+	patch="$ROOT/patches/$1/$2"
+	if ! git apply --reverse --check "$patch" 2> /dev/null; then
+		echo "applaying patch $patch"
+		git apply "$patch" || exit 1
+	else
+		echo "patch $patch already applied"
+	fi
+}
+
+if [[ $CMD != "--update" ]]; then
+	# Выполняем синхронизацию сабмодулей
+	git submodule sync
+	# Инициализируем подпроекты
+	git submodule update --init --recursive
+fi
+
+# Сборка AWH
+src="$ROOT/submodules/awh"
+if [ ! -f "$src/.stamp_done" ]; then
+	printf "\n****** ANYKS AWH ******\n"
+	cd "$src" || exit 1
+
+	# Версия AWH
+	VER="4.2.3"
+
+	# Переключаемся на main
+	git checkout main
+	# Выполняем удаление предыдущей закаченной версии
+	git tag -d v${VER}
+	# Закачиваем все теги
+	git fetch --all --tags
+	# Удаляем старую ветку
+	git branch -D v${VER}-branch
+	# Выполняем переключение на указанную версию
+	git checkout -b v${VER}-branch v${VER}
+
+	if [[ $CMD = "--update" ]]; then
+		# Выполняем обновление репозитория
+		git pull origin main
+	else
+		# Выполняем конфигурацию проекта
+		if [[ $OS = "Windows" ]]; then
+			# Выполняем сборку зависимостей
+			./build_third_party.sh
+		else
+			# Выполняем сборку зависимостей
+			./build_third_party.sh --idn
+		fi
+	fi
+
+	# Создаём каталог сборки
+	mkdir -p "build" || exit 1
+	# Переходим в каталог
+	cd "build" || exit 1
+
+	# Удаляем старый файл кэша
+	rm -rf "$src/build/CMakeCache.txt"
+
+	# Выполняем конфигурацию проекта
+	if [[ $OS = "Windows" ]]; then
+		cmake \
+		 -DCMAKE_BUILD_IDN="ON" \
+		 -DCMAKE_BUILD_TYPE=Release \
+		 -DCMAKE_SYSTEM_NAME=Windows \
+		 -DCMAKE_INSTALL_PREFIX="$PREFIX" \
+		 -G "MSYS Makefiles" \
+		 .. || exit 1
+	else
+		cmake \
+		 -DCMAKE_BUILD_IDN="ON" \
+		 -DCMAKE_BUILD_TYPE=Release \
+		 -DCMAKE_INSTALL_PREFIX="$PREFIX" \
+		 .. || exit 1
+	fi
+
+	# Выполняем сборку на всех логических ядрах
+	make -j"$numproc" || exit 1
+	# Выполняем установку проекта
+	make install || exit 1
+
+	# Помечаем флагом, что сборка и установка произведена
+	touch "$src/.stamp_done"
+	cd "$ROOT" || exit 1
+fi
+
+# Сборка YAML
+src="$ROOT/submodules/yaml"
+if [ ! -f "$src/.stamp_done" ]; then
+	printf "\n****** YAML ******\n"
+	cd "$src" || exit 1
+
+	# Версия YAML
+	VER="0.8.0"
+
+	# Переключаемся на main
+	git checkout main
+	# Выполняем удаление предыдущей закаченной версии
+	git tag -d v${VER}
+	# Закачиваем все теги
+	git fetch --all --tags
+	# Удаляем старую ветку
+	git branch -D v${VER}-branch
+	# Выполняем переключение на указанную версию
+	git checkout -b v${VER}-branch ${VER}
+
+	if [[ $CMD = "--update" ]]; then
+		# Выполняем обновление репозитория
+		git pull origin main
+	fi
+
+	# Создаём каталог сборки
+	mkdir -p "build" || exit 1
+	# Переходим в каталог
+	cd "build" || exit 1
+
+	# Удаляем старый файл кэша
+	rm -rf "$src/build/CMakeCache.txt"
+
+	# Выполняем конфигурацию проекта
+	if [[ $OS = "Windows" ]]; then
+		cmake \
+		 -DCMAKE_BUILD_TYPE=Release \
+		 -DCMAKE_SYSTEM_NAME=Windows \
+		 -DYAML_CPP_BUILD_TOOLS=OFF \
+		 -DYAML_BUILD_SHARED_LIBS=OFF \
+		 -DYAML_CPP_BUILD_CONTRIB=OFF \
+		 -DCMAKE_INSTALL_PREFIX="$PREFIX" \
+		 -G "MSYS Makefiles" \
+		 .. || exit 1
+	else
+		cmake \
+		 -DCMAKE_BUILD_TYPE=Release \
+		 -DYAML_CPP_BUILD_TOOLS=OFF \
+		 -DYAML_BUILD_SHARED_LIBS=OFF \
+		 -DYAML_CPP_BUILD_CONTRIB=OFF \
+		 -DCMAKE_INSTALL_PREFIX="$PREFIX" \
+		 .. || exit 1
+	fi
+
+	# Выполняем сборку на всех логических ядрах
+	make -j"$numproc" || exit 1
+	# Выполняем установку проекта
+	make install || exit 1
+
+	# Помечаем флагом, что сборка и установка произведена
+	touch "$src/.stamp_done"
+	cd "$ROOT" || exit 1
+fi
+
+# Сборка LibXML2
+src="$ROOT/submodules/libxml2"
+if [ ! -f "$src/.stamp_done" ]; then
+ 	printf "\n****** GNU LibXML2 ******\n"
+ 	cd "$src" || exit 1
+
+	# Версия LibXML2
+	VER="2.13.3"
+
+	# Выполняем удаление предыдущей закаченной версии
+	git tag -d v${VER}
+	# Закачиваем все изменения
+	git fetch --all
+	# Закачиваем все теги
+	git fetch --all --tags
+	# Выполняем жесткое переключение на master
+	git reset --hard origin/master
+	# Переключаемся на master
+	git checkout master
+	# Выполняем обновление данных
+	git pull origin master
+	# Удаляем старую ветку
+	git branch -D v${VER}-branch
+	# Выполняем переключение на указанную версию
+	git checkout -b v${VER}-branch v${VER}
+
+	# Создаём каталог сборки
+	mkdir -p "build" || exit 1
+	# Переходим в каталог
+	cd "build" || exit 1
+
+	# Удаляем старый файл кэша
+	rm -rf "$src/build/CMakeCache.txt"
+
+	# Выполняем конфигурацию проекта
+	if [[ $OS = "Windows" ]]; then
+		cmake \
+		 -DBUILD_SHARED_LIBS="OFF" \
+		 -DLIBXML2_WITH_LZMA="OFF" \
+		 -DLIBXML2_WITH_ZLIB="OFF" \
+		 -DLIBXML2_WITH_ICONV="OFF" \
+		 -DLIBXML2_WITH_PYTHON="OFF" \
+		 -DCMAKE_BUILD_TYPE=Release \
+		 -DCMAKE_SYSTEM_NAME=Windows \
+		 -DCMAKE_INSTALL_PREFIX="$PREFIX" \
+		 -G "MSYS Makefiles" \
+		 .. || exit 1
+	else
+		cmake \
+		 -DBUILD_SHARED_LIBS="OFF" \
+		 -DLIBXML2_WITH_LZMA="OFF" \
+		 -DLIBXML2_WITH_ZLIB="OFF" \
+		 -DLIBXML2_WITH_ICONV="OFF" \
+		 -DLIBXML2_WITH_PYTHON="OFF" \
+		 -DCMAKE_BUILD_TYPE=Release \
+		 -DCMAKE_INSTALL_PREFIX="$PREFIX" \
+		 .. || exit 1
+	fi
+
+	# Выполняем сборку на всех логических ядрах
+	make -j"$numproc" || exit 1
+	# Выполняем установку проекта
+	make install || exit 1
+
+	# Помечаем флагом, что сборка и установка произведена
+	touch "$src/.stamp_done"
+	cd "$ROOT" || exit 1
+fi
+
+# Если CMAKE-файл уже существует
+if [ -f "$ROOT/cmake/FindAWH.cmake" ]; then
+	# Выполняем удаление ранее созданный CMAKE-файл
+	rm "$ROOT/cmake/FindAWH.cmake" || exit 1
+fi
+
+# Если операционной системой является Windows
+if [ $OS = "Windows" ]; then
+	# Выполняем создание символьной ссылки CMAKE-файла
+	ln -s "$PREFIX/cmake/FindAWH.cmake" "$ROOT/cmake/FindAWH.cmake" || exit 1
+# Если операционная система является nix-подобной
+else
+	# Выполняем создание символьной ссылки CMAKE-файла
+	ln -f "$PREFIX/cmake/FindAWH.cmake" "$ROOT/cmake/FindAWH.cmake" || exit 1
+fi
+
+# Переименовываем расширение библиотек для Windows
+if [ $OS = "Windows" ]; then # Windows
+	for i in $(ls "$PREFIX/lib" | grep .a$);
+	do
+		mv "$PREFIX/lib/$i" "$PREFIX/lib/$(basename "$i" .a).lib"
+	done
+fi
+
+printf "\n****************************************"
+printf "\n************   Success!!!   ************"
+printf "\n****************************************"
+printf "\n\n\n"
---
+++ cmake/FindAWH.cmake
@@ -0,0 +1,285 @@
+set(CMAKE_FIND_USE_SYSTEM_ENVIRONMENT_PATH FALSE)
+
+# Если операцинная система относится к MS Windows
+if(${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
+    set(CMAKE_FIND_LIBRARY_PREFIXES "lib")
+    set(CMAKE_FIND_LIBRARY_SUFFIXES ".a")
+endif()
+
+# Поиск пути к заголовочным файлам
+find_path(LZ4_INCLUDE_DIR NAMES lz4.h PATHS /Users/forman/Work/GIT/acu/third_party/include/lz4 NO_DEFAULT_PATH)
+find_path(BZ2_INCLUDE_DIR NAMES bzlib.h PATHS /Users/forman/Work/GIT/acu/third_party/include/bz2 NO_DEFAULT_PATH)
+find_path(ZSTD_INCLUDE_DIR NAMES zstd.h PATHS /Users/forman/Work/GIT/acu/third_party/include/zstd NO_DEFAULT_PATH)
+find_path(LZMA_INCLUDE_DIR NAMES lzma.h PATHS /Users/forman/Work/GIT/acu/third_party/include/lzma NO_DEFAULT_PATH)
+find_path(ZLIB_INCLUDE_DIR NAMES zlib.h PATHS /Users/forman/Work/GIT/acu/third_party/include/zlib NO_DEFAULT_PATH)
+find_path(CITY_INCLUDE_DIR NAMES cityhash/city.h PATHS /Users/forman/Work/GIT/acu/third_party/include NO_DEFAULT_PATH)
+find_path(JSON_INCLUDE_DIR NAMES json.hpp PATHS /Users/forman/Work/GIT/acu/third_party/include/nlohmann NO_DEFAULT_PATH)
+find_path(AWH_INCLUDE_DIR NAMES server/awh.hpp PATHS /Users/forman/Work/GIT/acu/third_party/include/awh NO_DEFAULT_PATH)
+find_path(BROTLI_INCLUDE_ENCODE_DIR NAMES encode.h PATHS /Users/forman/Work/GIT/acu/third_party/include/brotli NO_DEFAULT_PATH)
+find_path(BROTLI_INCLUDE_DECODE_DIR NAMES decode.h PATHS /Users/forman/Work/GIT/acu/third_party/include/brotli NO_DEFAULT_PATH)
+find_path(OPENSSL_INCLUDE_DIR NAMES openssl/opensslconf.h PATHS /Users/forman/Work/GIT/acu/third_party/include NO_DEFAULT_PATH)
+find_path(PCRE_INCLUDE_DIR NAMES pcre2.h PATHS /Users/forman/Work/GIT/acu/third_party/include/pcre2 NO_DEFAULT_PATH)
+find_path(NGHTTP2_INCLUDE_DIR NAMES nghttp2.h PATHS /Users/forman/Work/GIT/acu/third_party/include/nghttp2 NO_DEFAULT_PATH)
+
+# Если операцинная система не относится к MS Windows
+if(NOT ${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
+    find_path(JEMALLOC_INCLUDE_DIR NAMES jemalloc.h PATHS /Users/forman/Work/GIT/acu/third_party/include/jemalloc NO_DEFAULT_PATH)
+endif()
+
+# Сборка модуля AWH_IDN, если операционной системой не является Windows
+if(CMAKE_BUILD_IDN AND (NOT ${CMAKE_SYSTEM_NAME} STREQUAL "Windows"))
+    find_path(IDN2_INCLUDE_DIR NAMES idn2.h PATHS /Users/forman/Work/GIT/acu/third_party/include/idn2 NO_DEFAULT_PATH)
+    find_path(ICONV_INCLUDE_DIR NAMES iconv.h PATHS /Users/forman/Work/GIT/acu/third_party/include/iconv NO_DEFAULT_PATH)
+endif()
+
+# Поиск библиотеки AWH
+find_library(AWH_LIBRARY NAMES awh PATHS /Users/forman/Work/GIT/acu/third_party/lib NO_DEFAULT_PATH)
+
+# Если активирован режим отладки
+if(CMAKE_AWH_BUILD_DEBUG)
+    # Поиск библиотеки AWH
+    find_library(DEPEND_LIBRARY NAMES dependence PATHS /Users/forman/Work/GIT/acu/third_party/lib NO_DEFAULT_PATH)
+endif()
+
+# Подключаем 'FindPackageHandle' для использования модуля поиска (find_package(<PackageName>))
+include(FindPackageHandleStandardArgs)
+
+# Сборка модуля AWH_IDN, если операционной системой не является Windows
+if(CMAKE_BUILD_IDN AND (NOT ${CMAKE_SYSTEM_NAME} STREQUAL "Windows"))
+    # Если активирован режим отладки
+    if(CMAKE_AWH_BUILD_DEBUG)
+        # Выполняем проверку на существование зависимостей
+        find_package_handle_standard_args(AWH REQUIRED_VARS
+            DEPEND_LIBRARY
+            AWH_LIBRARY
+            LZ4_INCLUDE_DIR
+            BZ2_INCLUDE_DIR
+            ZSTD_INCLUDE_DIR
+            LZMA_INCLUDE_DIR
+            ZLIB_INCLUDE_DIR
+            BROTLI_INCLUDE_ENCODE_DIR
+            BROTLI_INCLUDE_DECODE_DIR
+            AWH_INCLUDE_DIR
+            CITY_INCLUDE_DIR
+            OPENSSL_INCLUDE_DIR
+            PCRE_INCLUDE_DIR
+            JEMALLOC_INCLUDE_DIR
+            NGHTTP2_INCLUDE_DIR
+            JSON_INCLUDE_DIR
+            IDN2_INCLUDE_DIR
+            ICONV_INCLUDE_DIR
+
+            FAIL_MESSAGE "AWH library is not found"
+        )
+        # Формируем список библиотек
+        set(AWH_LIBRARIES ${DEPEND_LIBRARY} ${AWH_LIBRARY})
+    # Если режим отладки не активирован
+    else()
+        # Выполняем проверку на существование зависимостей
+        find_package_handle_standard_args(AWH REQUIRED_VARS
+            AWH_LIBRARY
+            LZ4_INCLUDE_DIR
+            BZ2_INCLUDE_DIR
+            ZSTD_INCLUDE_DIR
+            LZMA_INCLUDE_DIR
+            ZLIB_INCLUDE_DIR
+            BROTLI_INCLUDE_ENCODE_DIR
+            BROTLI_INCLUDE_DECODE_DIR
+            AWH_INCLUDE_DIR
+            CITY_INCLUDE_DIR
+            OPENSSL_INCLUDE_DIR
+            PCRE_INCLUDE_DIR
+            JEMALLOC_INCLUDE_DIR
+            NGHTTP2_INCLUDE_DIR
+            JSON_INCLUDE_DIR
+            IDN2_INCLUDE_DIR
+            ICONV_INCLUDE_DIR
+
+            FAIL_MESSAGE "AWH library is not found"
+        )
+        # Формируем список библиотек
+        set(AWH_LIBRARIES ${AWH_LIBRARY})
+    endif()
+    # Формируем список заголовочных файлов
+    set(AWH_INCLUDE_DIRS
+        ${LZ4_INCLUDE_DIR}
+        ${BZ2_INCLUDE_DIR}
+        ${ZSTD_INCLUDE_DIR}
+        ${LZMA_INCLUDE_DIR}
+        ${ZLIB_INCLUDE_DIR}
+        ${BROTLI_INCLUDE_ENCODE_DIR}
+        ${AWH_INCLUDE_DIR}
+        ${CITY_INCLUDE_DIR}
+        ${OPENSSL_INCLUDE_DIR}
+        ${PCRE_INCLUDE_DIR}
+        ${JEMALLOC_INCLUDE_DIR}
+        ${NGHTTP2_INCLUDE_DIR}
+        ${JSON_INCLUDE_DIR}
+        ${IDN2_INCLUDE_DIR}
+        ${ICONV_INCLUDE_DIR}
+    )
+    # Выполняем установку указанного списка заголовочных файлов зависимостей
+    install(DIRECTORY "${IDN2_INCLUDE_DIR}" DESTINATION "${CMAKE_INSTALL_PREFIX}/include" FILES_MATCHING PATTERN "*.h")
+    install(DIRECTORY "${ICONV_INCLUDE_DIR}" DESTINATION "${CMAKE_INSTALL_PREFIX}/include" FILES_MATCHING PATTERN "*.h")
+# Если операцинная система относится к MS Windows
+elseif(${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
+    # Если активирован режим отладки
+    if(CMAKE_AWH_BUILD_DEBUG)
+        # Выполняем проверку на существование зависимостей
+        find_package_handle_standard_args(AWH REQUIRED_VARS
+            DEPEND_LIBRARY
+            AWH_LIBRARY
+            LZ4_INCLUDE_DIR
+            BZ2_INCLUDE_DIR
+            ZSTD_INCLUDE_DIR
+            LZMA_INCLUDE_DIR
+            ZLIB_INCLUDE_DIR
+            BROTLI_INCLUDE_ENCODE_DIR
+            BROTLI_INCLUDE_DECODE_DIR
+            AWH_INCLUDE_DIR
+            CITY_INCLUDE_DIR
+            OPENSSL_INCLUDE_DIR
+            PCRE_INCLUDE_DIR
+            NGHTTP2_INCLUDE_DIR
+            JSON_INCLUDE_DIR
+
+            FAIL_MESSAGE "AWH library is not found"
+        )
+        # Формируем список библиотек
+        set(AWH_LIBRARIES ${DEPEND_LIBRARY} ${AWH_LIBRARY})
+    # Если режим отладки не активирован
+    else()
+        # Выполняем проверку на существование зависимостей
+        find_package_handle_standard_args(AWH REQUIRED_VARS
+            AWH_LIBRARY
+            LZ4_INCLUDE_DIR
+            BZ2_INCLUDE_DIR
+            ZSTD_INCLUDE_DIR
+            LZMA_INCLUDE_DIR
+            ZLIB_INCLUDE_DIR
+            BROTLI_INCLUDE_ENCODE_DIR
+            BROTLI_INCLUDE_DECODE_DIR
+            AWH_INCLUDE_DIR
+            CITY_INCLUDE_DIR
+            OPENSSL_INCLUDE_DIR
+            PCRE_INCLUDE_DIR
+            NGHTTP2_INCLUDE_DIR
+            JSON_INCLUDE_DIR
+
+            FAIL_MESSAGE "AWH library is not found"
+        )
+        # Формируем список библиотек
+        set(AWH_LIBRARIES ${AWH_LIBRARY})
+    endif()
+    # Формируем список заголовочных файлов
+    set(AWH_INCLUDE_DIRS
+        ${LZ4_INCLUDE_DIR}
+        ${BZ2_INCLUDE_DIR}
+        ${ZSTD_INCLUDE_DIR}
+        ${LZMA_INCLUDE_DIR}
+        ${ZLIB_INCLUDE_DIR}
+        ${BROTLI_INCLUDE_ENCODE_DIR}
+        ${AWH_INCLUDE_DIR}
+        ${CITY_INCLUDE_DIR}
+        ${OPENSSL_INCLUDE_DIR}
+        ${PCRE_INCLUDE_DIR}
+        ${NGHTTP2_INCLUDE_DIR}
+        ${JSON_INCLUDE_DIR}
+    )
+# Если операцинная система относится к Nix-подобной
+else()
+    # Если активирован режим отладки
+    if(CMAKE_AWH_BUILD_DEBUG)
+        # Выполняем проверку на существование зависимостей
+        find_package_handle_standard_args(AWH REQUIRED_VARS
+            DEPEND_LIBRARY
+            AWH_LIBRARY
+            LZ4_INCLUDE_DIR
+            BZ2_INCLUDE_DIR
+            ZSTD_INCLUDE_DIR
+            LZMA_INCLUDE_DIR
+            ZLIB_INCLUDE_DIR
+            BROTLI_INCLUDE_ENCODE_DIR
+            BROTLI_INCLUDE_DECODE_DIR
+            AWH_INCLUDE_DIR
+            CITY_INCLUDE_DIR
+            OPENSSL_INCLUDE_DIR
+            PCRE_INCLUDE_DIR
+            JEMALLOC_INCLUDE_DIR
+            NGHTTP2_INCLUDE_DIR
+            JSON_INCLUDE_DIR
+
+            FAIL_MESSAGE "AWH library is not found"
+        )
+        # Формируем список библиотек
+        set(AWH_LIBRARIES ${DEPEND_LIBRARY} ${AWH_LIBRARY})
+    # Если режим отладки не активирован
+    else()
+        # Выполняем проверку на существование зависимостей
+        find_package_handle_standard_args(AWH REQUIRED_VARS
+            AWH_LIBRARY
+            LZ4_INCLUDE_DIR
+            BZ2_INCLUDE_DIR
+            ZSTD_INCLUDE_DIR
+            LZMA_INCLUDE_DIR
+            ZLIB_INCLUDE_DIR
+            BROTLI_INCLUDE_ENCODE_DIR
+            BROTLI_INCLUDE_DECODE_DIR
+            AWH_INCLUDE_DIR
+            CITY_INCLUDE_DIR
+            OPENSSL_INCLUDE_DIR
+            PCRE_INCLUDE_DIR
+            JEMALLOC_INCLUDE_DIR
+            NGHTTP2_INCLUDE_DIR
+            JSON_INCLUDE_DIR
+
+            FAIL_MESSAGE "AWH library is not found"
+        )
+        # Формируем список библиотек
+        set(AWH_LIBRARIES ${AWH_LIBRARY})
+    endif()
+    # Формируем список заголовочных файлов
+    set(AWH_INCLUDE_DIRS
+        ${LZ4_INCLUDE_DIR}
+        ${BZ2_INCLUDE_DIR}
+        ${ZSTD_INCLUDE_DIR}
+        ${LZMA_INCLUDE_DIR}
+        ${ZLIB_INCLUDE_DIR}
+        ${BROTLI_INCLUDE_ENCODE_DIR}
+        ${AWH_INCLUDE_DIR}
+        ${CITY_INCLUDE_DIR}
+        ${OPENSSL_INCLUDE_DIR}
+        ${PCRE_INCLUDE_DIR}
+        ${JEMALLOC_INCLUDE_DIR}
+        ${NGHTTP2_INCLUDE_DIR}
+        ${JSON_INCLUDE_DIR}
+    )
+endif()
+
+# Устанавливаем библиотеку AWH
+install(FILES ${AWH_LIBRARY} DESTINATION "${CMAKE_INSTALL_PREFIX}/lib")
+
+# Если активирован режим отладки
+if(CMAKE_AWH_BUILD_DEBUG)
+    # Устанавливаем статическую библиотеку
+    install(FILES ${DEPEND_LIBRARY} DESTINATION "${CMAKE_INSTALL_PREFIX}/lib")    
+endif()
+
+# Выполняем установку оставшихся заголовочных файлов зависимостей
+install(DIRECTORY "${AWH_INCLUDE_DIR}" DESTINATION "${CMAKE_INSTALL_PREFIX}/include" FILES_MATCHING PATTERN "*.hpp")
+install(DIRECTORY "${JSON_INCLUDE_DIR}" DESTINATION "${CMAKE_INSTALL_PREFIX}/include" FILES_MATCHING PATTERN "*.hpp")
+install(DIRECTORY "${LZ4_INCLUDE_DIR}" DESTINATION "${CMAKE_INSTALL_PREFIX}/include" FILES_MATCHING PATTERN "*.h")
+install(DIRECTORY "${BZ2_INCLUDE_DIR}" DESTINATION "${CMAKE_INSTALL_PREFIX}/include" FILES_MATCHING PATTERN "*.h")
+install(DIRECTORY "${ZSTD_INCLUDE_DIR}" DESTINATION "${CMAKE_INSTALL_PREFIX}/include" FILES_MATCHING PATTERN "*.h")
+install(DIRECTORY "${LZMA_INCLUDE_DIR}" DESTINATION "${CMAKE_INSTALL_PREFIX}/include" FILES_MATCHING PATTERN "*.h")
+install(DIRECTORY "${ZLIB_INCLUDE_DIR}" DESTINATION "${CMAKE_INSTALL_PREFIX}/include" FILES_MATCHING PATTERN "*.h")
+install(DIRECTORY "${PCRE_INCLUDE_DIR}" DESTINATION "${CMAKE_INSTALL_PREFIX}/include" FILES_MATCHING PATTERN "*.h")
+install(DIRECTORY "${NGHTTP2_INCLUDE_DIR}" DESTINATION "${CMAKE_INSTALL_PREFIX}/include" FILES_MATCHING PATTERN "*.h")
+install(DIRECTORY "${BROTLI_INCLUDE_ENCODE_DIR}" DESTINATION "${CMAKE_INSTALL_PREFIX}/include" FILES_MATCHING PATTERN "*.h")
+install(DIRECTORY "${CITY_INCLUDE_DIR}/cityhash" DESTINATION "${CMAKE_INSTALL_PREFIX}/include" FILES_MATCHING PATTERN "*.h")
+install(DIRECTORY "${OPENSSL_INCLUDE_DIR}/openssl" DESTINATION "${CMAKE_INSTALL_PREFIX}/include" FILES_MATCHING PATTERN "*.h")
+
+# Если операцинная система не относится к MS Windows
+if(NOT ${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
+    install(DIRECTORY "${JEMALLOC_INCLUDE_DIR}" DESTINATION "${CMAKE_INSTALL_PREFIX}/include" FILES_MATCHING PATTERN "*.h")
+endif()
